11fce96ec2cf7df1878f86f82cb907bf10232243
Pierre-Emmanuel Goffi <chapsag@gmail.com>
2021-04-12 16:09:48 -0400

    ok

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include <signal.h>
 #include <string>
 #include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 
 #include "getkey.cc"
 #include "pcslib.h"
 #include "msglib.h"
+#include "memlib.h"
+#include "semlib.h"
+
+
 using namespace std;
 
 typedef void Sigfunc(int);
@@ -40,19 +46,32 @@ cout << "Non-- je ne veux pas etre suspendu" << endl;
 int main()
 {
  
+
+//char *tab = "gofp2701";
+//key_t mdrccajemetue = getkey(tab);
+//Port port1 = Port( mdrccajemetue);
+
+Pcs p_server;
+Pcs p_client[50];
+
+cout << "Starting";
+p_server.Fork("server");
+
+cout << "Starting" << 50 << "clients";
+
+for (int i = 0; i < 50; i++) {
+    p_client[i].Fork("client");
+}
+
+
+
+
 // on veut empecher l'arret du processus par un crtl-z
 signal(SIGTSTP,suspendre) ;
 // on veut empecher la destruction du processus par on crtl-c
 signal(SIGINT,fin) ;
 cout << "Le programme principal démarre ..........." << endl;
 // on boucle a l'infini
-    while(1) {
-    
-
-    char tab[] = "gofp2701";
-    Port port1 = Port(getkey(tab));
-
-
-    }   
+    while(1);
 }
 
