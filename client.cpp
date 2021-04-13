f5a9dcad4f5848bf73a62230cec75cea74189138
Pierre-Emmanuel Goffi <chapsag@gmail.com>
2021-04-12 21:14:41 -0400

    clean code

--- client.cpp
+++ client.cpp
@@ -34,39 +34,6 @@ int error = errno;
 //short htons(short value); local -> reseau
 //short ntohs(short value); reseau -> local
 
-/*void * clientThread(void * arg) 
-{
-
-    cout << "In thread" << endl;
-
-    char message[1000];
-    char buffer[1024];
-    int clientSocket;
-    struct sockaddr_in serverAddr;
-    socklen_t addr_size;
-
-    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(7799);
-    serverAddr.sin_addr.s_addr = inet_addr("localhost");
-    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-
-    addr_size = sizeof serverAddr;
-    connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-    strcpy(message, "Hello");
-
-    if( send(clientSocket, message, strlen(message), 0) < 0)
-        cout << "send failed" << endl;
-    
-    if ( recv(clientSocket, buffer, 1024, 0) < 0)
-        cout << "receive failed" << endl;
-    
-    cout << "Data received" << buffer << endl;
-
-    close(clientSocket);
-    pthread_exit(NULL);
-}*/
 
 int main() 
 {   
@@ -79,7 +46,6 @@ int main()
 
     msgbuf msg;
 
-    //msg.m_texte = "P";
     msg.m_type = 1;
 
     port.Envoie(&msg, sizeof(msg)); 
@@ -101,52 +67,6 @@ int main()
     }
 
     fclose(fd);
-
-
-    /*int clientSocket;
-    int port = 6666;
-    const char* ipadrress = "127.0.0.1";
-
-
-    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    
-    sockaddr_in server;
-    server.sin_addr.s_addr = inet_addr(ipadrress);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-
-    sockaddr_in addr = {0};
-    socklen_t len = sizeof(addr);
-    
-    // int newClient = accept(server, (sockaddr*)&addr, &len);
-
-    if (connect(clientSocket, (struct sockaddr *) &server, sizeof(server)) != 0) {
-        cout << "This is a problem" << endl;
-    }
-
-    cout << "Socket connected" << endl;
-    close(clientSocket); */
-
-
-
-    /*int i = 0;
-    pthread_t tId[51];
-
-    while(i < 50) 
-    {
-        if( pthread_create(&tId[i], NULL, clientThread, NULL) != 0)
-            cout << "Failed to create thread" << endl;
-            i++;
-    }
-     
-    sleep(20);
-    i = 0;
-
-    while (i<50)
-    {
-        pthread_join(tId[i++], NULL);
-        cout << i << endl;
-    }*/
     
     return 0; 
 }
--- serveur.cpp
+++ serveur.cpp
@@ -19,32 +19,7 @@
 using namespace std;
 
 
-//pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-
-//Sem mutex1;
-
-/*void * socketThread(void *arg) 
-{
-    int newSocket = *((int *)arg);
-    recv(newSocket, client_message, 2000, 0); //use to get a message from a socket
-
-
-    //pthread_mutex_lock(&mutex1); // Entrée Section critique
-    char *message = (char*) malloc(sizeof(client_message)+20);
-    strcpy(message, "Hello Client: ");
-    strcat(message, client_message);
-    strcat(message, "\n");
-    strcpy(buffer, message);
-    free(message);
-    //pthread_mutex_unlock(&mutex1); // Sortie section critique
-
-    sleep(1);
-
-    send(newSocket, buffer,13, 0);
-    cout << "Exit socketThread \n" << endl;
-    close(newSocket);
-    pthread_exit(NULL);
-}*/
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 int main() 
 {   
@@ -55,8 +30,6 @@ int main()
 
 
     msgbuf msg;
-
-    //msg.m_texte = "P";
     msg.m_type = 1;
 
     while (true) {
@@ -74,108 +47,5 @@ int main()
         fclose(fd);
     }
     
-
-// Copie -> temps de la copie pour pas de changement pdt la copie
-
-    
-
-
-   /* mutex1 = Sem(getkey("gofp2701"),1);
-    char *message = "Hello";
-    int i = 1;
-
-    mutex1.P();
-    strcpy(message, "Hello Client: ");
-    strcat(message, client_message);
-    strcat(message, "\n");
-    strcpy(buffer, message);
-    free(message);
-    mutex1.V(); */
-
-
-    // Pcs(/*Code du serveur à lancer*/)
-
-
-
-
-    /*int server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    if(server != -1) {
-        cout << "Socket error" << endl;
-    }
-
-    const short port = 9999;
-    sockaddr_in addr;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(port);
-    addr.sin_family = AF_INET;
-
-    int res = bind(server,(sockaddr*) &addr, sizeof(addr));
-    if(res != -1){
-        cout << "Binding error" << endl;
-    }
-
-    res = listen(server,SOMAXCONN);
-    if (res != -1) {
-        cout << "Listen error" << endl;
-    }
-
-    cout << "Serveur démarre sur le port " << port << endl;
-
-    sockaddr_in from = {0};
-    socklen_t addrlen = sizeof(addr);
-    int newClient = accept(server, (sockaddr*) &from, &addrlen);
-    
-    if (newClient != -1) {
-        cout << "Connection reussi" << endl;
-    }
-
-    close(server);
-    return 0;*/
-
-
-   /* int serverSocket, newSocket;
-
-    struct sockaddr_in serverAddr;
-    struct sockaddr_storage serverStorage;
-    socklen_t addr_size;
-
-
-    serverSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port   = htons(7799);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-    bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-
-    if(listen(serverSocket, 50)==0)
-        cout << "Listening" << endl;
-    else
-        cout << "Error..." << endl;
-    
-    pthread_t tId[60];
-    int i = 0;
-
-    while(1)
-    {
-        addr_size = sizeof serverStorage;
-        newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
-
-        if( pthread_create(&tId[i++], NULL, socketThread, &newSocket) !=0)
-            cout << "Faile dto create thread" << endl; 
-
-        if( i >= 50)
-        {
-          i = 0;
-          while(i < 50)
-          {
-            pthread_join(tId[i++],NULL);
-          }
-          i = 0;
-        }
-    }*/
-
     return 0;
 }
