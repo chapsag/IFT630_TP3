a2f7c519435bf1f76735bd088e860094197d4e24
Pierre-Emmanuel Goffi <chapsag@gmail.com>
2021-04-12 21:15:54 -0400

    errno

--- client.cpp
+++ client.cpp
@@ -1,15 +1,3 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <fcntl.h> // for open
-#include <unistd.h> // for close
-#include<pthread.h>
-#include <iostream>
-#include <errno.h>
-
 #include <iostream>
 #include <string>
 #include <cstring>
